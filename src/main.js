f27cd318f447f4134b5783bf19b58ec8f8732d69
Valerie Bragg <vbragg@ufledu>
2019-07-22 23:10:05 -0400

    Add setTimeout and correct JavaScript for verified functionality

--- src/main.js
+++ src/main.js
@@ -4,36 +4,25 @@ const moveBoxesRight = document.getElementsByClassName('moveoutright');
 const moveBoxesLeft = document.getElementsByClassName('moveoutleft');
 const rightBoxes = document.getElementById('moveright');
 const leftBoxes = document.getElementById('moveleft');
-const moveBoxes = document.getElementsByClassName('move');
 
 //Our Function
 function loopThrough() {
 
   const changeSpace = () => {
     leftBoxes.classList.toggle('leftover');
-    rightBoxes.classList.toggle('rightover');
-  }
+  };
 
   for(let i =0; i < this.textContent.length * 2; i++){
-    setTimeout(changeSpace, i * 1000);
+    setTimeout(changeSpace, i * 400);
   };
 };
 
 
 //Event Listener
-for (let i = 0; i < moveBoxesRight.length; i++) {
-  moveBoxesRight[i].addEventListener('click', loopThrough);
-};
-
 for (let i = 0; i < moveBoxesLeft.length; i++) {
   moveBoxesLeft[i].addEventListener('click', loopThrough);
 };
 
-/*
-for (let i = o; i < leftBoxes.length; i++) {
-  leftBoxes[i].addEventListener('click',loopThrough);
+for (let i = 0; i < moveBoxesRight.length; i++) {
+  moveBoxesRight[i].addEventListener('click', loopThrough);
 };
-*/
-
-
-//WANT TO TOGGLE BACK AND FORTH ON FLEXSTRETCH BETWEEN JUSTIFY CONTENT CENTER AND JUSTIFY CONTENT SPACE-BETWEEN
